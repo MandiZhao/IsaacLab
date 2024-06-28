895a82194c978e9794325bc65e2d614e57bc101e
Mandi <mandi.zhao@berkeley.edu>
2024-06-28 16:34:10 -0700

    fixed mimic import issue

--- source/standalone/chiral/test_inspire.py
+++ source/standalone/chiral/test_inspire.py
@@ -10,6 +10,7 @@ from omni.isaac.lab.app import AppLauncher
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Test inspire hand in Isaac Sim.")
 parser.add_argument("--num_envs", type=int, default=2, help="Number of environments to spawn.")
+# parser.add_argument("--replicate_physics", action="store_true", default=False, help="Replicate physics.")
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -50,16 +51,42 @@ class InspireTwoHandsSceneCfg(InteractiveSceneCfg):
 
     # inspire hand 
     inspire_left: ArticulationCfg = INSPIRE_HAND_LEFT_CFG.replace(prim_path="{ENV_REGEX_NS}/HandLeft")
-    inspire_right: ArticulationCfg = INSPIRE_HAND_RIGHT_CFG.replace(prim_path="{ENV_REGEX_NS}/HandRight")
+    # inspire_right: ArticulationCfg = INSPIRE_HAND_RIGHT_CFG.replace(prim_path="{ENV_REGEX_NS}/HandRight")
 
     # object
-    task_object = RigidObjectCfg(
-        prim_path="{ENV_REGEX_NS}/TaskObject",
-        spawn=sim_utils.UsdFileCfg(
-            usd_path="/home/mandi/IsaacLab/source/standalone/chiral/waffleiron.usd"
-        ),
-        init_state=RigidObjectCfg.InitialStateCfg(),
-    )
+    # task_object = RigidObjectCfg(
+    #     prim_path="{ENV_REGEX_NS}/TaskObject",
+    #     spawn=sim_utils.UsdFileCfg(
+    #         usd_path="/home/mandi/IsaacLab/source/standalone/chiral/waffleiron.usd"
+    #     ),
+    #     init_state=RigidObjectCfg.InitialStateCfg(),
+    # )
+    replicate_physics = False
+
+@torch.jit.script
+def scale(x, lower, upper):
+    return 0.5 * (x + 1.0) * (upper - lower) + lower
+
+def sample_actuated_joints(robot, actuator_key="fingers"):
+    actuated_indices = robot.actuators[actuator_key].joint_indices # list of int 
+    actuated_names = robot.actuators[actuator_key].joint_names # list of str
+    # get idx for joint "L_FF_J1"
+    idxs = []
+    for i, name in enumerate(actuated_names):
+        if "F_J" in name:
+            idxs.append(i) 
+
+    dof_limits = robot.root_physx_view.get_dof_limits() # shape [num_envs, num_total_joints, 2]
+    actuated_limits = dof_limits[:, idxs] # shape [num_envs, num_actuated_joints, 2]
+    # actions are in the range [-1, 1], sample uniformly
+    rand_actions = torch.rand(actuated_limits.shape[0], actuated_limits.shape[1])
+    # set to max value
+    # rand_actions = torch.ones_like(rand_actions)
+    joint_pos = scale(rand_actions, actuated_limits[:, :, 0], actuated_limits[:, :, 1])
+
+    # set joint positions
+    robot.set_joint_position_target(joint_pos, idxs)
+    return 
 
 def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
     """Runs the simulation loop."""
@@ -67,15 +94,15 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
     # note: we only do this here for readability.
     # robot = scene["inspire_left"]
     left_hand = scene["inspire_left"]
-    right_hand = scene["inspire_right"]
+    robot = left_hand
+    # right_hand = scene["inspire_right"]
     # Define simulation stepping
     sim_dt = sim.get_physics_dt()
     count = 0
     # Simulation loop
     while simulation_app.is_running():
         # Reset
-        if count % 500 == 0:
-            
+        if count % 500 == 0: 
             # reset counter
             count = 0
             # reset the scene entities
@@ -94,10 +121,13 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
             # clear internal buffers
             # scene.reset()
             # print("[INFO]: Resetting robot state...")
-
+            # sample actuated joints
+            print("Set new joint positions...   ")
+        if count % 20 == 0:
+            sample_actuated_joints(left_hand)
         # Apply random action
         # -- generate random joint efforts
-        efforts = torch.randn_like(left_hand.data.joint_pos) * 5.0
+        # efforts = torch.randn_like(left_hand.data.joint_pos) * 5.0
         # -- apply action to the robot
         # left_hand.set_joint_effort_target(efforts)
         # right_hand.set_joint_effort_target(efforts)
